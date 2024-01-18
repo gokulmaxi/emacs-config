84cc73e62fddd5b8164a90f20f4577d049142677
Pradeep Ragul S <pradeepraguls@presidio.com>
2024-01-18 16:34:34 +0530

    spr-version

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 (global-set-key [(hyper z)] 'undo)
 
 ;; mac switch meta key
-(defun mac-switch-meta nil 
+(defun mac-switch-meta nil
   "switch meta between Option and Command"
   (interactive)
   (if (eq mac-option-modifier nil)
@@ -17,7 +17,7 @@
 	(setq mac-option-modifier 'meta)
 	(setq mac-command-modifier 'hyper)
 	)
-    (progn 
+    (progn
       (setq mac-option-modifier nil)
       (setq mac-command-modifier 'meta)
       )
@@ -29,25 +29,24 @@
   "config.org"
   user-emacs-directory))
 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(completion-cycle-threshold 3)
- '(corfu-auto t)
- '(corfu-auto-delay 0.1)
- '(corfu-auto-prefix 2)
- '(corfu-preview-current t)
+ '(company-minimum-prefix-length 1)
  '(custom-safe-themes
-   '("f5f80dd6588e59cfc3ce2f11568ff8296717a938edd448a947f9823a4e282b66" default))
- '(indent-bars-treesit-support t)
- '(lsp-terraform-ls-prefill-required-fields t)
- '(package-selected-packages '(corfu doom-themes))
- '(tab-always-indent 'complete))
+   '("81f53ee9ddd3f8559f94c127c9327d578e264c574cda7c6d9daddaec226f87bb" "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0" "df6dfd55673f40364b1970440f0b0cb8ba7149282cf415b81aaad2d98b0f0290" "be84a2e5c70f991051d4aaf0f049fa11c172e5d784727e0b525565bb1533ec78" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "4990532659bb6a285fee01ede3dfa1b1bdf302c5c3c8de9fad9b6bc63a9252f7" "38c0c668d8ac3841cb9608522ca116067177c92feeabc6f002a27249976d7434" "02d422e5b99f54bd4516d4157060b874d14552fe613ea7047c4a5cfa1288cf4f" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" default))
+ '(indent-bars-treesit-support t t)
+ '(lsp-yaml-schema-store-uri
+   "https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/main/service-schema.json")
+ '(package-selected-packages '(doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
